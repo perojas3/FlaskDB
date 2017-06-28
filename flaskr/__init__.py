6798af8bd8aed2e7c0db107dda80c073e91193bc
perojas3 <perojas3@uc.cl>
2017-06-28 19:31:43 -0400

    get

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -51,7 +51,7 @@ def home():
 
 @app.route("/mongo", methods=['GET', 'POST'])
 def mongo():
-    query = request.args.post("query")
+    query = request.args.get("query")
     results = eval('mongodb.'+query)
     results = json_util.dumps(results, sort_keys=True, indent=4)
     if "find" in query:
